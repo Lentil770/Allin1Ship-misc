fc20c381c286edcde564f248e1856aa0ee7e19c2
Lentil770 <you@example.com>
2021-06-10 09:46:19 -0400

    auto push

--- GitAutoPushConstant.bat
+++ GitAutoPushConstant.bat
@@ -2,7 +2,7 @@
 	
 	:: Navigate to the directory you wish to push to GitHub
 	::Change <path> as needed. Eg. C:\Users\allin1ship\Allin1ship-Files
-	cd [filepath]\Allin1ship-Files
+	cd C:\Users\mushk\Allin1Ship\Allin1ship-Files
 	
 	::Pull any external changes (maybe you deleted a file from your repo?)
 	git pull
